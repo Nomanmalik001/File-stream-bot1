013d06292bc889499ab76ff87034f5b88738f141
Sahidmalik001 <106477157+Sahidmalik001@users.noreply.github.com>
2023-03-19 20:14:59 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/Sahidmalik001/FileStreamBot.git /FileStreamBot
+cd /FileStreamBot
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 main.py
